1f70dda25e0e6f1f39888659fdf0e280d9bf6eab
Nick Maniutin <maniutin@gmail.com>
2021-02-04 20:23:20 +0000

    uses map for the first time

---
+++ mapTest.js
@@ -0,0 +1,3 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+let lengths = lighthouses.map(length => length.length);
+console.log(lengths)
